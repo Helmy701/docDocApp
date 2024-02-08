045c5357dab026e2428495adee12f4d924374f14
Helmy701 <ahmedhelmy1632000@gmail.com>
2024-02-08 07:36:57 +0200

    first commit

---
+++ lib/doc_app.dart
@@ -0,0 +1,33 @@
+import 'package:docdoc/core/routes/routes.dart';
+import 'package:docdoc/core/theming/colors.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+import 'package:docdoc/core/routes/app_router.dart';
+
+class DocApp extends StatelessWidget {
+  final AppRouter appRouter;
+  const DocApp({super.key, required this.appRouter});
+
+  @override
+  Widget build(BuildContext context) {
+    return ScreenUtilInit(
+      designSize: const Size(360, 690),
+      minTextAdapt: true,
+      splitScreenMode: true,
+      // Use builder only if you need to use library outside ScreenUtilInit context
+      builder: (_, child) {
+        return MaterialApp(
+          title: 'Doc App',
+          theme: ThemeData(
+            primaryColor: ColorsTheme.mainBlue,
+            scaffoldBackgroundColor: Colors.white,
+          ),
+          debugShowCheckedModeBanner: false,
+          initialRoute: Routes.onBoardingScreen,
+          onGenerateRoute: appRouter.generateRoute,
+        );
+      },
+    );
+  }
+}
